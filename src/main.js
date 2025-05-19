c5af90bf23c18d006e57a1d062ee48ab8685f215
Piotr Porzuczek <pporzuczek@lyreco.com>
2025-05-19 22:34:09 +0200

    Clean up

--- src/main.js
+++
@@ -1,8 +0,0 @@
-    "data-component-path": "src/main.tsx",
-    "data-component-line": "5",
-    "data-component-file": "main.tsx",
-    "data-component-name": "App",
-    "data-component-content": "%7B%7D",
-  }),
-);
-export { $h as g };
