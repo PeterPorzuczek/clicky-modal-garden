6c09f709c675883fc1d882b559f6577d654d0569
Piotr Porzuczek <peter-porzuczek@outlook.com>
2025-05-20 17:21:26 +0200

    Fix product state updates

--- src/components/order/ProductSelectionStep.jsx
+++ src/components/order/ProductSelectionStep.jsx
@@ -38,7 +38,9 @@ export default function ProductSelectionStep({
   };
 
   const updateProduct = (id, field, value) => {
-    setProducts(products.map(p => (p.id === id ? { ...p, [field]: value } : p)));
+    setProducts(prev =>
+      prev.map(p => (p.id === id ? { ...p, [field]: value } : p))
+    );
   };
 
   return (
